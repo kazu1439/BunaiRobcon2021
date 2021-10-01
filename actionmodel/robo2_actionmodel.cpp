64485b67e6cc8acbd70cdb0f1a5539a8a14ad7af
kosakaasumi <83442606+kosakaasumi@users.noreply.github.com>
2021-10-02 01:47:24 +0900

    Update robo2_actionmodel.cpp

--- actionmodel/robo2_actionmodel.cpp
+++ actionmodel/robo2_actionmodel.cpp
@@ -1,7 +1,4 @@
 /**********************************************************************
-部内ロボコン　機体２　運動モデル
-**********************************************************************/
-/**********************************************************************
    Include Libraries
 **********************************************************************/
 #include "ros/ros.h"
@@ -18,10 +15,10 @@
 #define CYCLE_PERIOD 0.02f
 
 std::vector<float> vx_vy_vw = {0.0f, 0.0f, 0.0f}; //xの速度、yの速度、ω
-float ruto3 = 1.7320508;
-float l = 19.27;　　　　　　　　　　　　　　　 　　　//中心からホイールまでの距離
-std_msgs::Float32MultiArray msg_three_speed; 　　 //v0,v1,v2を格納 
-float mata[3] = {0.0f, 0.0f, 0.0f}; 　
+float ruto3 = 1.7320508;                          //√3
+float l = 19.27;                                  //中心からホイールまでの距離
+std_msgs::Float32MultiArray msg_three_speed;      //v0,v1,v2を格納 
+float mata[3] = {0.0f, 0.0f, 0.0f}; 
 /**********************************************************************
    Proto_type_Declare functions(受け取る関数)
 **********************************************************************/
@@ -32,7 +29,7 @@ void three_speed_cb(const std_msgs::Float32MultiArray::ConstPtr &msg);
 
 int main(int argc, char **argv)
 {
-   ros::init(argc, argv, "robo2_actionmodel");
+   ros::init(argc, argv, "actionmodel_omuni");
    ros::NodeHandle nh;
    ros::Subscriber sub_second = nh.subscribe("joy_content", 100, three_speed_cb);         //joyコンの値を受け取る
    ros::Publisher pub_float = nh.advertise<std_msgs::Float32MultiArray>("v0_v1_v2", 300); //mbedへ
@@ -41,12 +38,12 @@ int main(int argc, char **argv)
 
    while (ros::ok())
    {
-      ros::spinOnce();                                  
-      Eigen::Matrix<float, 3, 1> x;           //xの速度、yの速度、ω
+      ros::spinOnce();                                  //callback関数を読み込む  
+      Eigen::Matrix<float, 3, 1> x;                     //xの速度、yの速度、ω
       x << vx_vy_vw[0],
            vx_vy_vw[1],
            vx_vy_vw[2];
-      Eigen::Matrix<float, 3, 3> A;           //運動モデルの行列式のやつ
+      Eigen::Matrix<float, 3, 3> A;                     //運動モデルの行列式のやつ
       A << 1.0/2, -(ruto3/2),  l,
            1.0/2, ruto3/2,     l,
            1,         0,     l;
